6484cfadabe6eb05f16fc3a0c39c4dfc0cf11d6a
Jakub Słoniec <jakub.sloniec@gmail.com>
2022-04-14 20:56:38 +0200

    repackage

--- src/main/java/pl/sloniec/App.java
+++ src/main/java/pl/sloniec/App.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package pl.sloniec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
-
 }
